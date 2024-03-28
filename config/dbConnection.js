c0bde309a182b823090be7147f56e72a22511970
Benish <benish.p@isolve.global>
2024-03-28 11:39:14 +0530

    db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
+const PORT=5001
+const CONNECTION_STRING="mongodb+srv://Benish:iSolve123@mongo-server.etl1fjh.mongodb.net/mycontacts-backend"
 const connectDb = async() =>{
    try{
-   const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
-      useNewUrlParser: true,
-       useUnifiedTopology: true,
-  });
+   const connect = await mongoose.connect(CONNECTION_STRING);
    console.log('DataBase Connected :' , connect.connection.host,connect.connection.name);
    }catch(error){
     console.log(error,'error');
